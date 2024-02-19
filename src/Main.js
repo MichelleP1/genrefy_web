8090c3ca070990676bff7a618ca89a2b61f6600c
Michelle Peters <michellepeters94@gmail.com>
2024-02-19 14:41:13 -0600

    Update styling

--- src/Main.js
+++ src/Main.js
@@ -1,27 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
 import Player from "./Player/Player";
-import Browse from "./Player/Browse";
-import { Button } from "./Player/Button";
 
 function Main(props) {
   const { token, setToken } = props;
-  const [showPlayer, setShowPlayer] = useState(true);
-
-  const handleToggleStyle = () => {
-    setShowPlayer(!showPlayer);
-  };
 
   return (
     <>
-      <Button
-        title={showPlayer ? "Browse" : "Random"}
-        onClick={handleToggleStyle}
-      />
-      {showPlayer ? (
-        <Player token={token} setToken={setToken} />
-      ) : (
-        <Browse token={token} setToken={setToken} />
-      )}
+      <Player token={token} setToken={setToken} />
     </>
   );
 }
